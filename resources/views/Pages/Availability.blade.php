9d4c6bacf94eae8796c857bc5c2ef54f6ca5fef0
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-07-01 18:47:13 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Pages/Availability.blade.php
+++ resources/views/Pages/Availability.blade.php
@@ -2586,7 +2586,7 @@
                 <br>
                 <h2>
                     <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z"/></svg> L.T.T 
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z"/></svg> BIG DATA 
                     </span> 
                     <span>DEPASA MARINE</span>
                 </h2>
@@ -2611,7 +2611,7 @@
                 </table> <br>
                 <h2>
                     <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z"/></svg> SEA DREDGING
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z"/></svg> LIVE
                     </span> 
                     <span>
                         @if ($STARTDATE == date('Y-m-d'))
