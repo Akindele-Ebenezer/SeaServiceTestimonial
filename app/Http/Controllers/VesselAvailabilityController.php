debd80ea777ae0a4ac758f0001041f668d7eb652
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-07-08 08:10:08 +0100

    PRODUCTION MODE - Version 1.0.0

--- app/Http/Controllers/VesselAvailabilityController.php
+++ app/Http/Controllers/VesselAvailabilityController.php
@@ -25,7 +25,8 @@ class VesselAvailabilityController extends Controller
                                 ->where(function($query) {
                                     $query->where('StartDate', '>=', date('Y-m-d'))
                                             ->orWhere('EndDate', '>=', date('Y-m-d'));
-                                }) 
+                                })
+                                ->orWhere('EndDate', '<', date('Y-m-d'))
                                 ->groupBy('Vessel')->get(); 
         $NumberOfVessels_BUNKERY = VesselAvailability::select('Vessel')->where('Status', 'BUNKERY')->where('EndDate', '>=', date('Y-m-d'))->groupBy('Vessel')->get();
         $NumberOfVessels_INSPECTION = VesselAvailability::select('Vessel')->where('Status', 'INSPECTION')->where('EndDate', '>=', date('Y-m-d'))->groupBy('Vessel')->get();
