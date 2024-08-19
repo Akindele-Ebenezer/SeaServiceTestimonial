009ad5cd3f77079d8c44edcd848e9c967f02e5cb
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-08-19 09:10:45 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Charts/Chart3-JS.blade.php
+++ resources/views/Components/Charts/Chart3-JS.blade.php
@@ -70,7 +70,7 @@
         points: [ 
             @foreach($Vessel_ as $Vessel)
                 @php  
-                    $Period_Start = \DB::table('vessel_availabilities')->where('Vessel', $Vessel->VesselName)->where('Status', $Status)->where('StartDate', '>=', $StartDate_)->where('StartDate', '<=', $EndDate_)->orderBy('EndDate', 'DESC')->first();  
+                    $Period_Start = \DB::table('vessel_availabilities')->where('Vessel', $Vessel->VesselName)->where('Status', $Status)->where('StartDate', '>=', $StartDate_)->where('StartDate', '<=', $EndDate_)->orderBy('StartDate', 'DESC')->first();  
                     if ((empty($Period_Start)) || ($StartDate_ > ($Period_Start->StartDate ?? 'null'))) {
                         $Period_Start = \DB::table('vessel_availabilities')->where('Vessel', $Vessel->VesselName)->where('Status', $Status)->where('StartDate', '<=', $StartDate_)->where('EndDate', '>=', $StartDate_)->orderBy('StartDate', 'DESC')->first(); 
                     }
@@ -78,7 +78,7 @@
                     $StartDateTime = \Carbon\Carbon::parse(($Period_Start->StartDate ?? date('Y-m-d')) . ' ' . ($Period_Start->StartTime ?? '00:00'));
                     $EndDateTime = \Carbon\Carbon::parse(($Period_End->EndDate ?? date('Y-m-d')) . ' ' . ($Period_End->EndTime ?? '00:00'));
                     if (empty($Period_Start) || empty($Period_End)) {
-                        $TotalDays = 110;
+                        $TotalDays = 0;
                     } else if($Period_Start->StartDate == $Period_End->EndDate) {
                         $TotalDays = 1;
                     } else {
