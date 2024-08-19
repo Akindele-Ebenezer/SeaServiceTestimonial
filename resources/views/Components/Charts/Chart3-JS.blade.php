82d4adab1b2c33caf3d90432c798e2b7e37a5a3b
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-08-19 09:39:20 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Charts/Chart3-JS.blade.php
+++ resources/views/Components/Charts/Chart3-JS.blade.php
@@ -74,6 +74,7 @@
                     if ((empty($Period_Start)) || ($StartDate_ > ($Period_Start->StartDate ?? 'null'))) {
                         $Period_Start = \DB::table('vessel_availabilities')->where('Vessel', $Vessel->VesselName)->where('Status', $Status)->where('StartDate', '<=', $StartDate_)->where('EndDate', '>=', $StartDate_)->orderBy('StartDate')->first(); 
                     }
+                    print_r($Period_Start);
                     $Period_End = \DB::table('vessel_availabilities')->where('Vessel', $Vessel->VesselName)->where('Status', $Status)->where('EndDate', '>=', $StartDate_)->where('EndDate', '<=', $EndDate_)->orderBy('EndDate', 'DESC')->first(); 
                     $StartDateTime = \Carbon\Carbon::parse(($Period_Start->StartDate ?? date('Y-m-d')) . ' ' . ($Period_Start->StartTime ?? '00:00'));
                     $EndDateTime = \Carbon\Carbon::parse(($Period_End->EndDate ?? date('Y-m-d')) . ' ' . ($Period_End->EndTime ?? '00:00'));
