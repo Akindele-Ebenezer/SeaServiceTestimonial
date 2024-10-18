e6b25f048e8739d6d2ceaaefe9909e30fef27309
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-18 07:32:53 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php
+++ resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php
@@ -6,8 +6,8 @@ if (count($Periods) == 0) {
 }  
 $TotalDaysArr = [];
 foreach($Periods as $Period) { 
-    $StartDateTime = \Carbon\Carbon::parse(($Period->StartDate ?? date('Y-m-d')) . ' ' . ($Period->StartTime ?? '00:00'));
-    $EndDateTime = \Carbon\Carbon::parse(($Period->EndDate ?? date('Y-m-d')) . ' ' . ($Period->EndTime ?? '00:00')); 
+    // $StartDateTime = \Carbon\Carbon::parse(($Period->StartDate ?? date('Y-m-d')) . ' ' . ($Period->StartTime ?? '00:00'));
+    // $EndDateTime = \Carbon\Carbon::parse(($Period->EndDate ?? date('Y-m-d')) . ' ' . ($Period->EndTime ?? '00:00')); 
     if (
         ($Period->EndDate <= $EndDate_)) {
         $StartDateTime = \Carbon\Carbon::parse(($StartDate_ ?? date('Y-m-d')) . ' ' . ($Period->StartTime ?? '00:00'));
@@ -25,6 +25,5 @@ foreach($Periods as $Period) {
 $NumberOfTotalStatusForCurrentVessel = count(\DB::table('vessel_availabilities')->where('Vessel', $Vessel->VesselName)->whereBetween('StartDate', [$StartDate_, $EndDate_])->whereBetween('EndDate', [$StartDate_, $EndDate_])->get());
 if ($NumberOfTotalStatusForCurrentVessel == 0) {
     $NumberOfTotalStatusForCurrentVessel = 1;
-}
-$TotalDaysArr = [23];
+} 
 $PeriodicPercentageOfVesselAvailability = (round(count($Periods) / $NumberOfTotalStatusForCurrentVessel * 100, 0));
