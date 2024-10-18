3497aeb9eb6dfde83ce9363c3e6a60a89b4caefa
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-18 18:56:03 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php
+++ resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php
@@ -38,8 +38,8 @@ $Periods_ = \DB::table('vessel_availabilities')
 $TotalDaysArr = [];
 $TotalDays_PeriodArr = [];
 foreach($Periods as $Period) {  
-        $StartDateTime = \Carbon\Carbon::parse(($Period->StartDate ?? $StartDate_) . ' ' . ($Period->StartTime ?? '00:00'));
-        $EndDateTime = \Carbon\Carbon::parse(($Period->EndDate ?? $EndDate_) . ' ' . ($Period->EndTime ?? '00:00')); 
+    $StartDateTime = \Carbon\Carbon::parse(($StartDate_ ?? $StartDate_) . ' ' . ($Period->StartTime ?? '00:00'));
+    $EndDateTime = \Carbon\Carbon::parse(($EndDate_ ?? $EndDate_) . ' ' . ($Period->EndTime ?? '00:00')); 
 
     if (($Period->StartDate <= $StartDate_)) {
         $StartDateTime = \Carbon\Carbon::parse(($StartDate_ ?? date('Y-m-d')) . ' ' . ($Period->StartTime ?? '00:00'));
