71dfd42fd6f5d3912d6188e6f0cbda986572ba1a
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-22 14:18:08 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Charts/Chart5.blade.php
+++ resources/views/Components/Charts/Chart5.blade.php
@@ -7,22 +7,12 @@
           @if (isset($Vessels[0]))
           <canvas id="barChart"></canvas>
           @endif
-          {{-- @if (isset($Vessels[1]))
+          @if (isset($Vessels[1]))
           <canvas id="barChart1"></canvas>
           @endif
           @if (isset($Vessels[2]))
           <canvas id="barChart2"></canvas> 
-          @endif --}}
-        </div>
-    </div>
-    <div class="inner">
-        {{-- <p class="Close">✖</p>
-        <img class="OpenChartFilterButton" src="{{ asset('images/bar-chart.png') }}" alt="">  --}}
-        <h2>Vessel Availability — Bar Chart</h2>
-        <div> 
-          @if (isset($Vessels[1]))
-          <canvas id="barChart1"></canvas>
-          @endif 
+          @endif
         </div>
     </div>
   </div>
