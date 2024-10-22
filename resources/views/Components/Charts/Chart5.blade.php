e555426098fc13ced9cd512e26f9bcaebcf80216
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-22 15:33:18 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Charts/Chart5.blade.php
+++ resources/views/Components/Charts/Chart5.blade.php
@@ -2,11 +2,11 @@
     <div class="inner">
         {{-- <p class="Close">✖</p>
         <img class="OpenChartFilterButton" src="{{ asset('images/bar-chart.png') }}" alt="">  --}}
-        <h2>Vessel Availability — Bar Chart</h2>
+        <h2>Vessel Availability — {{ date('Y') }} Report</h2>
         <div> 
           <canvas id="barChart"></canvas>
           <canvas id="barChart1"></canvas>
-          <canvas id="barChart2"></canvas> 
+          {{-- <canvas id="barChart2"></canvas>  --}}
         </div>
     </div>
   </div>
