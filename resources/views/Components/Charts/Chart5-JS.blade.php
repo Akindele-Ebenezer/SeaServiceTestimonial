34cc4f4013873f0a6881fc2d02269171640528ef
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-22 15:08:10 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Charts/Chart5-JS.blade.php
+++ resources/views/Components/Charts/Chart5-JS.blade.php
@@ -116,218 +116,218 @@ options: {
 	}
 }); 
 @endif
-@if (isset($Vessels[1]))
-  let datasets1 = [{
-      label: 'Breakdown',
-			backgroundColor: "rgba(255,0,0,1)",
-      data: [ 
-        @php $Status = 'BREAKDOWN' @endphp
-            @foreach($Vessels[1] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Ready',
-			backgroundColor: "#45c490",
-      data: [ 
-        @php $Status = 'Idle' @endphp
-            @foreach($Vessels[1] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Bunkering',
-			backgroundColor: "#8a3ffc",
-      data: [ 
-        @php $Status = 'Bunkery' @endphp
-            @foreach($Vessels[1] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Maintenance',
-			backgroundColor: "#eee",
-      data: [ 
-        @php $Status = 'Maintenance' @endphp
-            @foreach($Vessels[1] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Inspection',
-			backgroundColor: "#ff832b",
-      data: [ 
-        @php $Status = 'Inspection' @endphp
-            @foreach($Vessels[1] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}]
-  let sums1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (let i = 0; i < sums1.length; i++) {
-    for (let dataset of datasets1) {
-        sums1[i] += dataset.data[i];
-      }
-    } 
-var ctx1 = document.getElementById("barChart1").getContext('2d');
-var barChart1 = new Chart(ctx1, {
-  type: 'bar', 
-	data: {
-        labels: [
-            @foreach($Vessels[1] as $Vessel)
-                "{{ substr($Vessel->VesselName, 0, 3) }}", 
-            @endforeach 
-        ],
-		datasets: datasets,
-	},
-options: {
-    tooltips: {
-      displayColors: true,
-      callbacks:{
-        mode: 'x',
-        label: function(tooltipItem, data) { 
-            const dataset = data.datasets[tooltipItem.datasetIndex];
-            const value = dataset.data[tooltipItem.index];
-            let Total = sums1[tooltipItem.index]; 
-            return 'Days: ' + value + ' (' + Math.round(((value/Total) * 100)) + '%)';
-        }
-      },
-    },
-    scales: {
-      x2: {
-        position: 'top',
-        labels: sums1    
-      },
-      xAxes: [{
-        stacked: true,
-        gridLines: {
-          display: false,
-        }
-      }],
-      yAxes: [{
-        stacked: true,
-        ticks: {
-          beginAtZero: true,
-        },
-        type: 'linear',
-      }]
-    },
-		responsive: true,
-		// maintainAspectRatio: false,
-		// legend: { position: 'bottom' },
-	}
-}); 
-@endif
-@if (isset($Vessels[2]))
-  let datasets2 = [{
-      label: 'Breakdown',
-			backgroundColor: "rgba(255,0,0,1)",
-      data: [ 
-        @php $Status = 'BREAKDOWN' @endphp
-            @foreach($Vessels[2] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Ready',
-			backgroundColor: "#45c490",
-      data: [ 
-        @php $Status = 'Idle' @endphp
-            @foreach($Vessels[2] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Bunkering',
-			backgroundColor: "#8a3ffc",
-      data: [ 
-        @php $Status = 'Bunkery' @endphp
-            @foreach($Vessels[2] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Maintenance',
-			backgroundColor: "#eee",
-      data: [ 
-        @php $Status = 'Maintenance' @endphp
-            @foreach($Vessels[2] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}, {
-			label: 'Inspection',
-			backgroundColor: "#ff832b",
-      data: [ 
-        @php $Status = 'Inspection' @endphp
-            @foreach($Vessels[2] as $Vessel)
-                @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
-                {{ array_sum($TotalDaysArr) }}, 
-            @endforeach  
-      ],
-		}]
-  let sums2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (let i = 0; i < sums2.length; i++) {
-    for (let dataset of datasets2) {
-        sums2[i] += dataset.data[i];
-      }
-    } 
-var ctx2 = document.getElementById("barChart2").getContext('2d');
-var barChart2 = new Chart(ctx2, {
-  type: 'bar', 
-	data: {
-        labels: [
-            @foreach($Vessels[2] as $Vessel)
-                "{{ substr($Vessel->VesselName, 0, 3) }}", 
-            @endforeach 
-        ],
-		datasets: datasets,
-	},
-options: {
-    tooltips: {
-      displayColors: true,
-      callbacks:{
-        mode: 'x',
-        label: function(tooltipItem, data) { 
-            const dataset = data.datasets[tooltipItem.datasetIndex];
-            const value = dataset.data[tooltipItem.index];
-            let Total = sums2[tooltipItem.index]; 
-            return 'Days: ' + value + ' (' + Math.round(((value/Total) * 100)) + '%)';
-        }
-      },
-    },
-    scales: {
-      x2: {
-        position: 'top',
-        labels: sums2    
-      },
-      xAxes: [{
-        stacked: true,
-        gridLines: {
-          display: false,
-        }
-      }],
-      yAxes: [{
-        stacked: true,
-        ticks: {
-          beginAtZero: true,
-        },
-        type: 'linear',
-      }]
-    },
-		responsive: true,
-		// maintainAspectRatio: false,
-		// legend: { position: 'bottom' },
-	}
-}); 
-@endif
+// @if (isset($Vessels[1]))
+//   let datasets1 = [{
+//       label: 'Breakdown',
+// 			backgroundColor: "rgba(255,0,0,1)",
+//       data: [ 
+//         @php $Status = 'BREAKDOWN' @endphp
+//             @foreach($Vessels[1] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Ready',
+// 			backgroundColor: "#45c490",
+//       data: [ 
+//         @php $Status = 'Idle' @endphp
+//             @foreach($Vessels[1] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Bunkering',
+// 			backgroundColor: "#8a3ffc",
+//       data: [ 
+//         @php $Status = 'Bunkery' @endphp
+//             @foreach($Vessels[1] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Maintenance',
+// 			backgroundColor: "#eee",
+//       data: [ 
+//         @php $Status = 'Maintenance' @endphp
+//             @foreach($Vessels[1] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Inspection',
+// 			backgroundColor: "#ff832b",
+//       data: [ 
+//         @php $Status = 'Inspection' @endphp
+//             @foreach($Vessels[1] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}]
+//   let sums1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//   for (let i = 0; i < sums1.length; i++) {
+//     for (let dataset of datasets1) {
+//         sums1[i] += dataset.data[i];
+//       }
+//     } 
+// var ctx1 = document.getElementById("barChart1").getContext('2d');
+// var barChart1 = new Chart(ctx1, {
+//   type: 'bar', 
+// 	data: {
+//         labels: [
+//             @foreach($Vessels[1] as $Vessel)
+//                 "{{ substr($Vessel->VesselName, 0, 3) }}", 
+//             @endforeach 
+//         ],
+// 		datasets: datasets,
+// 	},
+// options: {
+//     tooltips: {
+//       displayColors: true,
+//       callbacks:{
+//         mode: 'x',
+//         label: function(tooltipItem, data) { 
+//             const dataset = data.datasets[tooltipItem.datasetIndex];
+//             const value = dataset.data[tooltipItem.index];
+//             let Total = sums1[tooltipItem.index]; 
+//             return 'Days: ' + value + ' (' + Math.round(((value/Total) * 100)) + '%)';
+//         }
+//       },
+//     },
+//     scales: {
+//       x2: {
+//         position: 'top',
+//         labels: sums1    
+//       },
+//       xAxes: [{
+//         stacked: true,
+//         gridLines: {
+//           display: false,
+//         }
+//       }],
+//       yAxes: [{
+//         stacked: true,
+//         ticks: {
+//           beginAtZero: true,
+//         },
+//         type: 'linear',
+//       }]
+//     },
+// 		responsive: true,
+// 		// maintainAspectRatio: false,
+// 		// legend: { position: 'bottom' },
+// 	}
+// }); 
+// @endif
+// @if (isset($Vessels[2]))
+//   let datasets2 = [{
+//       label: 'Breakdown',
+// 			backgroundColor: "rgba(255,0,0,1)",
+//       data: [ 
+//         @php $Status = 'BREAKDOWN' @endphp
+//             @foreach($Vessels[2] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Ready',
+// 			backgroundColor: "#45c490",
+//       data: [ 
+//         @php $Status = 'Idle' @endphp
+//             @foreach($Vessels[2] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Bunkering',
+// 			backgroundColor: "#8a3ffc",
+//       data: [ 
+//         @php $Status = 'Bunkery' @endphp
+//             @foreach($Vessels[2] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Maintenance',
+// 			backgroundColor: "#eee",
+//       data: [ 
+//         @php $Status = 'Maintenance' @endphp
+//             @foreach($Vessels[2] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}, {
+// 			label: 'Inspection',
+// 			backgroundColor: "#ff832b",
+//       data: [ 
+//         @php $Status = 'Inspection' @endphp
+//             @foreach($Vessels[2] as $Vessel)
+//                 @php include('../resources/views/Components/Includes/PeriodicData_NumberOfDaysWorkedForEachVessel.php'); @endphp
+//                 {{ array_sum($TotalDaysArr) }}, 
+//             @endforeach  
+//       ],
+// 		}]
+//   let sums2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//   for (let i = 0; i < sums2.length; i++) {
+//     for (let dataset of datasets2) {
+//         sums2[i] += dataset.data[i];
+//       }
+//     } 
+// var ctx2 = document.getElementById("barChart2").getContext('2d');
+// var barChart2 = new Chart(ctx2, {
+//   type: 'bar', 
+// 	data: {
+//         labels: [
+//             @foreach($Vessels[2] as $Vessel)
+//                 "{{ substr($Vessel->VesselName, 0, 3) }}", 
+//             @endforeach 
+//         ],
+// 		datasets: datasets,
+// 	},
+// options: {
+//     tooltips: {
+//       displayColors: true,
+//       callbacks:{
+//         mode: 'x',
+//         label: function(tooltipItem, data) { 
+//             const dataset = data.datasets[tooltipItem.datasetIndex];
+//             const value = dataset.data[tooltipItem.index];
+//             let Total = sums2[tooltipItem.index]; 
+//             return 'Days: ' + value + ' (' + Math.round(((value/Total) * 100)) + '%)';
+//         }
+//       },
+//     },
+//     scales: {
+//       x2: {
+//         position: 'top',
+//         labels: sums2    
+//       },
+//       xAxes: [{
+//         stacked: true,
+//         gridLines: {
+//           display: false,
+//         }
+//       }],
+//       yAxes: [{
+//         stacked: true,
+//         ticks: {
+//           beginAtZero: true,
+//         },
+//         type: 'linear',
+//       }]
+//     },
+// 		responsive: true,
+// 		// maintainAspectRatio: false,
+// 		// legend: { position: 'bottom' },
+// 	}
+// }); 
+// @endif
 </script>
--- resources/views/Components/Charts/Chart5.blade.php
+++ resources/views/Components/Charts/Chart5.blade.php
@@ -3,19 +3,10 @@
         {{-- <p class="Close">✖</p>
         <img class="OpenChartFilterButton" src="{{ asset('images/bar-chart.png') }}" alt="">  --}}
         <h2>Vessel Availability — Bar Chart</h2>
-        <div>
-          @if (isset($Vessels[0]))
-          <canvas style="width: 1135px; height: 567px;" width="1135" height="567" id="barChart"></canvas>
-          {{-- <canvas id="barChart"></canvas> --}}
-          @endif
-          @if (isset($Vessels[1]))
-          <canvas style="width: 1135px; height: 567px;" width="1135" height="567" id="barChart1"></canvas>
-          {{-- <canvas id="barChart1"></canvas> --}}
-          @endif
-          @if (isset($Vessels[2]))
-          <canvas style="width: 1135px; height: 567px;" width="1135" height="567" id="barChart2"></canvas>
-          {{-- <canvas id="barChart2"></canvas>  --}}
-          @endif
+        <div> 
+          <canvas id="barChart"></canvas>
+          <canvas id="barChart1"></canvas>
+          <canvas id="barChart2"></canvas> 
         </div>
     </div>
   </div>
