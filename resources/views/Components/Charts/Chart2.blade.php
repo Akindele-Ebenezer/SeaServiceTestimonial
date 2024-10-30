e1292e1ace235c728b7db9ade710f2d27b4cd304
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-10-30 17:45:01 +0100

    PRODUCTION MODE - Version 1.0.0

--- resources/views/Components/Charts/Chart2.blade.php
+++ resources/views/Components/Charts/Chart2.blade.php
@@ -2,31 +2,25 @@
 <script src="https://www.google.com/jsapi"></script>
 <div class="col-md-6">
     <div id="chart_div1" class="chart-2"></div>
-</div>   
-@php
-    $LTT_NumberOfVessels_DOCKING = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'DOCKING')->where('vessels_vessel_information.Company', 'L.T.T')->get(); 
-    $LTT_NumberOfVessels_READY = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'IDLE')->where('vessels_vessel_information.Company', 'L.T.T')->get();
-    $LTT_NumberOfVessels_INSPECTION = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'INSPECTION')->where('vessels_vessel_information.Company', 'L.T.T')->get();
-    $LTT_NumberOfVessels_BREAKDOWN = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'BREAKDOWN')->where('vessels_vessel_information.Company', 'L.T.T')->get(); 
-    $LTT_NumberOfVessels_BUNKERY = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'BUNKERY')->where('vessels_vessel_information.Company', 'L.T.T')->get();
-    $LTT_NumberOfVessels_MAINTENANCE = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'MAINTENANCE')->where('vessels_vessel_information.Company', 'L.T.T')->get();
-
-    $DEPASA_NumberOfVessels_DOCKING = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'DOCKING')->where('vessels_vessel_information.Company', 'DEPASA')->get(); 
-    $DEPASA_NumberOfVessels_READY = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'IDLE')->where('vessels_vessel_information.Company', 'DEPASA')->get();
-    $DEPASA_NumberOfVessels_INSPECTION = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'INSPECTION')->where('vessels_vessel_information.Company', 'DEPASA')->get();
-    $DEPASA_NumberOfVessels_BREAKDOWN = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'BREAKDOWN')->where('vessels_vessel_information.Company', 'DEPASA')->get(); 
-    $DEPASA_NumberOfVessels_BUNKERY = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'BUNKERY')->where('vessels_vessel_information.Company', 'DEPASA')->get();
-    $DEPASA_NumberOfVessels_MAINTENANCE = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'MAINTENANCE')->where('vessels_vessel_information.Company', 'DEPASA')->get();
-
-@endphp
+</div>    
 <script>
     google.load("visualization", "1", {packages:["corechart"]});
     google.setOnLoadCallback(drawChart1);
     function drawChart1() {
     var data = google.visualization.arrayToDataTable([
         ['Company', 'DOCKING', 'BREAKDOWN', 'INSPECTION', 'READY ', 'BUNKERY', 'MAINTENANCE'],
-        ['L.T.T',  {{ count($LTT_NumberOfVessels_DOCKING) }}, {{ count($LTT_NumberOfVessels_BREAKDOWN) }}, {{ count($LTT_NumberOfVessels_INSPECTION) }}, {{ count($LTT_NumberOfVessels_READY) }}, {{ count($LTT_NumberOfVessels_BUNKERY) }}, {{ count($LTT_NumberOfVessels_MAINTENANCE) }}],
-        ['DEPASA',  {{ count($DEPASA_NumberOfVessels_DOCKING) }}, {{ count($DEPASA_NumberOfVessels_BREAKDOWN) }}, {{ count($DEPASA_NumberOfVessels_INSPECTION) }}, {{ count($DEPASA_NumberOfVessels_READY) }}, {{ count($DEPASA_NumberOfVessels_BUNKERY) }}, {{ count($DEPASA_NumberOfVessels_MAINTENANCE) }}],
+        @php $Companies = \DB::table('companies_')->get(); @endphp
+        @foreach($Companies as $Company)
+            @php
+                $Company_NumberOfVessels_DOCKING = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'DOCKING')->where('vessels_vessel_information.Company', $Company->Alias)->get(); 
+                $Company_NumberOfVessels_READY = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'IDLE')->where('vessels_vessel_information.Company', $Company->Alias)->get();
+                $Company_NumberOfVessels_INSPECTION = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'INSPECTION')->where('vessels_vessel_information.Company', $Company->Alias)->get();
+                $Company_NumberOfVessels_BREAKDOWN = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'BREAKDOWN')->where('vessels_vessel_information.Company', $Company->Alias)->get(); 
+                $Company_NumberOfVessels_BUNKERY = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'BUNKERY')->where('vessels_vessel_information.Company', $Company->Alias)->get();
+                $Company_NumberOfVessels_MAINTENANCE = \App\Models\VesselAvailability::select('Vessel')->join('vessels_vessel_information', 'vessels_vessel_information.VesselName', '=', 'vessel_availabilities.Vessel')->where('Status', 'MAINTENANCE')->where('vessels_vessel_information.Company', $Company->Alias)->get();
+            @endphp
+            ['{{ $Company->Alias }}',  {{ count($Company_NumberOfVessels_DOCKING) }}, {{ count($Company_NumberOfVessels_BREAKDOWN) }}, {{ count($Company_NumberOfVessels_INSPECTION) }}, {{ count($Company_NumberOfVessels_READY) }}, {{ count($Company_NumberOfVessels_BUNKERY) }}, {{ count($Company_NumberOfVessels_MAINTENANCE) }}],
+        @endforeach
     ]);
 
     var options = {
