a1fd7190f50331041f50ea83e8915c2458669a36
Akindele Ebenezer <tobi.akindele@gmail.com>
2024-11-22 17:17:41 +0100

    PRODUCTION MODE - Version 1.0.0

--- public/js/Pages/Login.js
+++ public/js/Pages/Login.js
@@ -5,7 +5,7 @@ let Login = document.querySelector('.Login');
 // 
 const images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg',
                 '7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg',
-                ,'13.jpg','14.jpg','15.jpg','16.jpeg','17.jpeg','18.jpeg',
+                '13.jpg','14.jpg','15.jpg','16.jpeg','17.jpeg','18.jpeg',
                 '19.jpeg','20.jpeg','21.jpeg','22.jpeg'];
 
 let currentIndex = 0;
